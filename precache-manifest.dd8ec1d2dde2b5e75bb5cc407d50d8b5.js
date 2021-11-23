5c4cbe68904463ad66feabc58b8b880edae596e0
AlejandroGaGa <alejandrogarciagallardo1234@gmail.com>
2021-11-23 10:18:50 -0600

    Primer commit después de trabajar localmente

---
+++ precache-manifest.dd8ec1d2dde2b5e75bb5cc407d50d8b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c967609b8290a97e6b177aed4f423df5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "394149c527759982aeb4",
+    "url": "/static/css/main.1c1e009e.chunk.css"
+  },
+  {
+    "revision": "3696c363641e190a7541",
+    "url": "/static/js/2.d54fc02e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d54fc02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394149c527759982aeb4",
+    "url": "/static/js/main.5e38bc38.chunk.js"
+  },
+  {
+    "revision": "ae594932184f88bfb438",
+    "url": "/static/js/runtime-main.9d9d98d4.js"
+  }
+]);
